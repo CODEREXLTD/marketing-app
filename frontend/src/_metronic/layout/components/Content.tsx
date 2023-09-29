76a10637591351f328eb981a0db8ea07a9c18a18
Tuhin <tohin@coderex.co>
2023-09-29 17:53:33 +0600

    pull feat/smtp

--- frontend/src/_metronic/layout/components/Content.tsx
+++ frontend/src/_metronic/layout/components/Content.tsx
@@ -1,16 +1,39 @@
 import clsx from 'clsx'
 import { FC, useEffect } from 'react'
 import { useLocation } from 'react-router'
+import { useAuth } from '../../../app/modules/auth'
+import { getUserByToken, refreshAuthToken } from '../../../app/modules/auth/core/_requests'
 import { DrawerComponent } from '../../assets/ts/components'
 import { WithChildren } from '../../helpers'
 import { useLayout } from '../core'
 
 const Content: FC<WithChildren> = ({children}) => {
-  const {classes} = useLayout()
-  const location = useLocation()
-  useEffect(() => {
-    DrawerComponent.hideAll()
-  }, [location])
+    const {classes} = useLayout()  
+    const location = useLocation()
+    const {auth, saveAuth, setCurrentUser} = useAuth()
+    useEffect(() => {
+        DrawerComponent.hideAll()
+    }, [location])
+
+    const refreshToken = async () => {        
+        const data = await refreshAuthToken(JSON.stringify({refresh:auth?.refreshToken}));  
+        saveAuth(data)
+        const {data: user} = await getUserByToken(data?.token)
+        setCurrentUser(user)
+    }
+
+    useEffect(()=>{        
+        const REFRESH_INTERVAL = 1000 * 60 * 19;
+        const interval = setInterval(()=>{
+            if(auth){
+                refreshToken()
+            }
+        }, REFRESH_INTERVAL)
+        return () => clearInterval(interval)
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[auth])
+
   return (
     <div id='kt_content_container' className={clsx(classes.contentContainer.join(' '))}>
       {children}
