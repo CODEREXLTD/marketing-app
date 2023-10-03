543be1c6ecab58a0852b9be45fec02e8fd1036a3
Tuhin <tohin@coderex.co>
2023-10-03 12:48:11 +0600

    enhance: clean dashboard

--- frontend/src/app/pages/dashboard/DashboardWrapper.tsx
+++ frontend/src/app/pages/dashboard/DashboardWrapper.tsx
@@ -1,18 +1,6 @@
 import { useEffect } from "react";
 import { useIntl } from "react-intl";
 import { PageLink, PageTitle } from "../../../_metronic/layout/core";
-import {
-    ListsWidget2,
-    ListsWidget3,
-    ListsWidget4,
-    ListsWidget6,
-    ListsWidget9,
-    MixedWidget3,
-    MixedWidget8,
-    StatisticsWidget4,
-    TablesWidget5,
-    TablesWidget9
-} from "../../../_metronic/partials/widgets";
 
 const dashboardBreadCrumbs: Array<PageLink> = [
   {
@@ -37,26 +25,25 @@ const DashboardPage = () => {
       {/* begin::Row */}
       <div className="row gy-5 g-xl-8">
         {/* begin::Col */}
-        <div className="col-xxl-4">
+        {/* <div className="col-xxl-4">
           <MixedWidget3
             className="card-xl-stretch mb-xl-8"
             chartColor="primary"
             chartHeight="250px"
           />
-        </div>
+        </div> */}
         {/* end::Col */}
 
         {/* begin::Col */}
-        <div className="col-xxl-8">
+        {/* <div className="col-xxl-8">
           <TablesWidget9 className="card-xxl-stretch mb-5 mb-xl-8" />
-        </div>
+        </div> */}
         {/* end::Col */}
       </div>
       {/* end::Row */}
 
       {/* begin::Row */}
-      <div className="row gy-5 g-xl-8">
-        {/* begin::Col */}
+      {/* <div className="row gy-5 g-xl-8">
         <div className="col-xxl-4">
           <StatisticsWidget4
             className="card-xxl-stretch-50 mb-5 mb-xl-8"
@@ -74,63 +61,41 @@ const DashboardPage = () => {
             change="+259"
           />
         </div>
-        {/* end::Col */}
 
-        {/* begin::Col */}
         <div className="col-xxl-4">
           <ListsWidget9 className="card-xxl-stretch mb-xl-8" />
         </div>
-        {/* end::Col */}
 
-        {/* begin::Col */}
         <div className="col-xxl-4">
           <ListsWidget4 className="card-xxl-stretch mb-5 mb-xl-8" />
         </div>
-        {/* end::Col */}
       </div>
-      {/* end::Row */}
 
-      {/* begin::Row */}
       <div className="row gy-5 g-xl-8">
-        {/* begin::Col */}
         <div className="col-xxl-4">
           <ListsWidget3 className="card-xxl-stretch mb-xl-3" />
         </div>
-        {/* end::Col */}
 
-        {/* begin::Col */}
         <div className="col-xxl-8">
           <TablesWidget9 className="card-xxl-stretch mb-5 mb-xl-8" />
         </div>
-        {/* end::Col */}
       </div>
-      {/* end::Row */}
 
-      {/* begin::Row */}
       <div className="row gy-5 g-xl-8">
-        {/* begin::Col */}
         <div className="col-xl-4">
           <ListsWidget2 className="card-xl-stretch mb-xl-8" />
         </div>
-        {/* end::Col */}
 
-        {/* begin::Col */}
         <div className="col-xl-4">
           <ListsWidget6 className="card-xl-stretch mb-xl-8" />
         </div>
-        {/* end::Col */}
 
-        {/* begin::Col */}
         <div className="col-xl-4">
           <ListsWidget4 className="card-xl-stretch mb-5 mb-xl-8" items={5} />
         </div>
-        {/* end::Col */}
       </div>
-      {/* end::Row */}
 
-      {/* begin::Row */}
       <div className="row g-5 gx-xxl-8">
-        {/* begin::Col */}
         <div className="col-xxl-4">
           <MixedWidget8
             className="card-xxl-stretch mb-xl-3"
@@ -138,15 +103,11 @@ const DashboardPage = () => {
             chartHeight="150px"
           />
         </div>
-        {/* end::Col */}
 
-        {/* begin::Col */}
         <div className="col-xxl-8">
           <TablesWidget5 className="card-xxl-stretch mb-5 mb-xxl-8" />
         </div>
-        {/* end::Col */}
-      </div>
-      {/* end::Row */}
+      </div> */}
     </>
   );
 };
