1cacf18d098ae795354e61d86ac07b7f4ff9ef64
sadi1991 <sadi@coderex.co>
2023-10-03 12:16:06 +0600

    remove frontend from docker

--- backend/closez/settings.py
+++ backend/closez/settings.py
@@ -61,7 +61,7 @@ AUTH_USER_MODEL = 'authentication.User'
 
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
+    # 'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
